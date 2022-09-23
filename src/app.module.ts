eb320fdb8d449c521540d47657337ecbffa4c6af
byun618 <byun618@gmail.com>
2022-09-23 21:15:02 +0900

    코드 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { BookmarkModule } from './bookmark/bookmark.module'
 
 @Module({
   imports: [
